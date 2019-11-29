d8bcadcfe68ba47c7d27cd293d8f69575ec9c008
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-11-29 19:18:22 +0200

    timer component and css added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Timer from "./components/Timer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
 }
 
 export default App;
